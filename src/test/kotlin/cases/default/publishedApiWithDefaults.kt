e1bbb22f18713cc1d63bb656cfb766791325aecd
Sebastian Ludwig <sebastian@lurado.de>
2021-03-28 19:06:53 +0200

    Remove optionals from existing test cases

--- src/test/kotlin/cases/default/publishedApiWithDefaults.kt
+++ src/test/kotlin/cases/default/publishedApiWithDefaults.kt
@@ -3,7 +3,9 @@
  * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
  */
 @file:Suppress("UNUSED_PARAMETER")
+
 package cases.default
 
 @PublishedApi
-internal fun twoDefaults(par0: Int, par1: Any = 1, par2: String? = null) {}
+internal fun twoDefaults(par0: Int, par1: Any = 1, par2: String = "") {
+}
--- src/test/kotlin/cases/protected/protectedInAbstract.kt
+++ src/test/kotlin/cases/protected/protectedInAbstract.kt
@@ -2,7 +2,7 @@ package cases.protected
 
 public abstract class PublicAbstractClass protected constructor() {
     protected abstract val protectedVal: Int
-    protected abstract var protectedVar: Any?
+    protected abstract var protectedVar: Any
 
     protected abstract fun protectedFun()
 }
