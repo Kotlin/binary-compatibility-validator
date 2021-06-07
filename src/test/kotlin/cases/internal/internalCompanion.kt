b5fcff2f60a0f931e3d4144f4b9595938633b142
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2021-06-07 19:21:38 +0300

    ~name companions properly, drop a comment

--- src/test/kotlin/cases/internal/internalCompanion.kt
+++ src/test/kotlin/cases/internal/internalCompanion.kt
@@ -7,15 +7,18 @@ package cases.internal
 
 // Internal companion is not part of public API
 // neither should be outer static final companion field
+
+// Named that way so the test ensures that PublicClassInternalCompanionWithNameShadowing does properly differentiate
+// 'Companion' static final field and hand-written field with the same type
 class Companion {
     internal companion object
 }
 
-class NamedCompanion {
+class PublicClassNamedInternalCompanion {
     internal companion object Foo
 }
 
-class NameShadowing {
+class PublicClassInternalCompanionWithNameShadowing {
     internal companion object Companion {
         @JvmStatic
         public val Companion: cases.internal.Companion = Companion()
