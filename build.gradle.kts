3bec20a8b52d8c738faa1591f7c9c9662224b4da
aSemy <897017+aSemy@users.noreply.github.com>
2023-03-22 11:29:55 +0100

    Update to Gradle 8 (#122)
    - bump Gradle Plugin Publish Plugin version
    - build script updates & improvements
    - use Java Toolchains to set the Java version
    - use jvm-test-fixtures plugin for functionalTest sources
    - update build config, remove redundant Maven publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,20 @@
 import com.gradle.publish.*
+import java.io.*
 import kotlinx.validation.build.*
+import org.gradle.api.attributes.TestSuiteType.FUNCTIONAL_TEST
+import org.gradle.api.internal.tasks.testing.*
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 import org.jetbrains.kotlin.gradle.tasks.*
+
 
 plugins {
     kotlin("jvm")
     `java-gradle-plugin`
-    id("com.gradle.plugin-publish") apply false
+    id("com.gradle.plugin-publish")
     signing
     `maven-publish`
-}
-
-repositories {
-    mavenCentral()
-    gradlePluginPortal()
-    google()
+    `jvm-test-suite`
 }
 
 sourceSets {
@@ -22,21 +23,6 @@ sourceSets {
     }
 }
 
-sourceSets {
-    create("functionalTest") {
-        withConvention(org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet::class) {
-        }
-        compileClasspath += sourceSets.main.get().output + configurations.testRuntimeClasspath
-        runtimeClasspath += output + compileClasspath
-    }
-}
-
-tasks.register<Test>("functionalTest") {
-    testClassesDirs = sourceSets["functionalTest"].output.classesDirs
-    classpath = sourceSets["functionalTest"].runtimeClasspath
-}
-tasks.check { dependsOn(tasks["functionalTest"]) }
-
 // While gradle testkit supports injection of the plugin classpath it doesn't allow using dependency notation
 // to determine the actual runtime classpath for the plugin. It uses isolation, so plugins applied by the build
 // script are not visible in the plugin classloader. This means optional dependencies (dependent on applied plugins -
@@ -44,7 +30,11 @@ tasks.check { dependsOn(tasks["functionalTest"]) }
 // extending the classpath. It is based upon: https://docs.gradle.org/6.0/userguide/test_kit.html#sub:test-kit-classpath-injection
 
 // Create a configuration to register the dependencies against
-val testPluginRuntimeConfiguration = configurations.register("testPluginRuntime")
+val testPluginRuntimeConfiguration = configurations.create("testPluginRuntime") {
+    isCanBeConsumed = false
+    isCanBeResolved = true
+    isVisible = false
+}
 
 // The task that will create a file that stores the classpath needed for the plugin to have additional runtime dependencies
 // This file is then used in to tell TestKit which classpath to use.
@@ -58,8 +48,11 @@ val createClasspathManifest = tasks.register("createClasspathManifest") {
         .withPropertyName("outputDir")
 
     doLast {
-        outputDir.mkdirs()
-        file(outputDir.resolve("plugin-classpath.txt")).writeText(testPluginRuntimeConfiguration.get().joinToString("\n"))
+        file(outputDir.resolve("plugin-classpath.txt")).writeText(
+            testPluginRuntimeConfiguration.joinToString(
+                "\n"
+            )
+        )
     }
 }
 
@@ -79,90 +72,124 @@ dependencies {
     implementation("org.ow2.asm:asm-tree:9.2")
     implementation("com.googlecode.java-diff-utils:diffutils:1.3.0")
     compileOnly("org.jetbrains.kotlin.multiplatform:org.jetbrains.kotlin.multiplatform.gradle.plugin:1.8.10")
-//    compileOnly("com.android.tools.build:gradle:${androidGradlePluginVersion}")
+    //compileOnly("com.android.tools.build:gradle:${androidGradlePluginVersion}")
 
     // The test needs the full kotlin multiplatform plugin loaded as it has no visibility of previously loaded plugins,
     // unlike the regular way gradle loads plugins.
-    add(testPluginRuntimeConfiguration.name, "org.jetbrains.kotlin.multiplatform:org.jetbrains.kotlin.multiplatform.gradle.plugin:$kotlinVersion")
-    add(testPluginRuntimeConfiguration.name, "com.android.tools.build:gradle:${androidGradlePluginVersion}")
-
-    testImplementation(kotlin("test-junit"))
-    "functionalTestImplementation"(files(createClasspathManifest))
-
-    "functionalTestImplementation"("org.assertj:assertj-core:3.18.1")
-    "functionalTestImplementation"(gradleTestKit())
-    "functionalTestImplementation"(kotlin("test-junit"))
+    testPluginRuntimeConfiguration("org.jetbrains.kotlin.multiplatform:org.jetbrains.kotlin.multiplatform.gradle.plugin:$kotlinVersion")
+    testPluginRuntimeConfiguration("com.android.tools.build:gradle:${androidGradlePluginVersion}")
 }
 
 tasks.compileKotlin {
-    kotlinOptions.apply {
-        allWarningsAsErrors = true
-
-        languageVersion = "1.4"
-        apiVersion = "1.4"
-        jvmTarget = "1.8"
-
+    compilerOptions {
+        allWarningsAsErrors.set(true)
+        @Suppress("DEPRECATION") // Compatibility with Gradle 7 requires Kotlin 1.4
+        languageVersion.set(KotlinVersion.KOTLIN_1_4)
+        apiVersion.set(languageVersion)
+        jvmTarget.set(JvmTarget.JVM_1_8)
         // Suppressing "w: Language version 1.4 is deprecated and its support will be removed" message
         // because LV=1.4 in practice is mandatory as it is a default language version in Gradle 7.0+ for users' kts scripts.
-        freeCompilerArgs += "-Xsuppress-version-warnings"
+        freeCompilerArgs.addAll(
+            "-Xsuppress-version-warnings"
+        )
     }
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    withJavadocJar()
+    withSourcesJar()
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
 }
 
-tasks {
-    compileTestKotlin {
-        kotlinOptions {
-            languageVersion = "1.6"
-        }
+tasks.compileTestKotlin {
+    compilerOptions {
+        languageVersion.set(KotlinVersion.KOTLIN_1_6)
     }
-    test {
-        systemProperty("overwrite.output", System.getProperty("overwrite.output", "false"))
-        systemProperty("testCasesClassesDirs", sourceSets.test.get().output.classesDirs.asPath)
-        jvmArgs("-ea")
-    }
+}
+
+tasks.withType<Test>().configureEach {
+    systemProperty("overwrite.output", System.getProperty("overwrite.output", "false"))
+    systemProperty("testCasesClassesDirs", sourceSets.test.get().output.classesDirs.asPath)
+    jvmArgs("-ea")
 }
 
 properties["DeployVersion"]?.let { version = it }
 
 publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
-            mavenCentralMetadata()
-            mavenCentralArtifacts(project, project.sourceSets.main.get().allSource)
-        }
+    mavenRepositoryPublishing(project)
+    mavenCentralMetadata()
 
-        mavenRepositoryPublishing(project)
-        mavenCentralMetadata()
-    }
-
-    publications.withType(MavenPublication::class).all {
+    publications.withType<MavenPublication>().all {
         signPublicationIfKeyPresent(this)
     }
+
+    // a publication will be created automatically by com.gradle.plugin-publish
 }
 
-apply(plugin = "org.gradle.java-gradle-plugin")
-apply(plugin = "com.gradle.plugin-publish")
-
-extensions.getByType(PluginBundleExtension::class).apply {
-    website = "https://github.com/Kotlin/binary-compatibility-validator"
-    vcsUrl = "https://github.com/Kotlin/binary-compatibility-validator"
-    tags = listOf("kotlin", "api-management", "binary-compatibility")
-}
-
+@Suppress("UnstableApiUsage")
 gradlePlugin {
-    testSourceSets(sourceSets["functionalTest"])
+    website.set("https://github.com/Kotlin/binary-compatibility-validator")
+    vcsUrl.set("https://github.com/Kotlin/binary-compatibility-validator")
+
+    plugins.configureEach {
+        tags.addAll("kotlin", "api-management", "binary-compatibility")
+    }
 
     plugins {
         create("binary-compatibility-validator") {
             id = "org.jetbrains.kotlinx.binary-compatibility-validator"
             implementationClass = "kotlinx.validation.BinaryCompatibilityValidatorPlugin"
             displayName = "Binary compatibility validator"
-            description = "Produces binary API dumps and compares them in order to verify that binary API is preserved"
+            description =
+                "Produces binary API dumps and compares them in order to verify that binary API is preserved"
         }
     }
+}
+
+@Suppress("UnstableApiUsage")
+testing {
+    suites {
+        withType<JvmTestSuite>().configureEach {
+            useJUnit()
+            dependencies {
+                implementation(project())
+                implementation("org.assertj:assertj-core:3.18.1")
+                implementation(project.dependencies.kotlin("test-junit").toString())
+            }
+        }
+
+        val test by getting(JvmTestSuite::class) {
+            description = "Regular unit tests"
+        }
+
+        val functionalTest by creating(JvmTestSuite::class) {
+            testType.set(FUNCTIONAL_TEST)
+            description = "Functional Plugin tests using Gradle TestKit"
+
+            dependencies {
+                implementation(files(createClasspathManifest))
+
+                implementation(gradleApi())
+                implementation(gradleTestKit())
+            }
+
+            targets.configureEach {
+                testTask.configure {
+                    shouldRunAfter(test)
+                }
+            }
+        }
+
+        gradlePlugin.testSourceSets(functionalTest.sources)
+
+        tasks.check {
+            dependsOn(functionalTest)
+        }
+    }
+}
+
+tasks.withType<Sign>().configureEach {
+    onlyIf("only sign if signatory is present") { signatory?.keyId != null }
 }
