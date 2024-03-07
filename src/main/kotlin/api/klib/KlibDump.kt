28cc85a9af0a3a1b9a69ccbbbf1c76273a64a2bd
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2024-03-07 17:50:17 +0100

    Improved kdoc and added samples

--- src/main/kotlin/api/klib/KlibDump.kt
+++ src/main/kotlin/api/klib/KlibDump.kt
@@ -10,30 +10,56 @@ import kotlinx.validation.klib.KlibAbiDumpMerger
 import java.io.File
 import java.io.FileNotFoundException
 
+/**
+ * Represents KLib ABI dump and allows manipulating it.
+ */
 @ExperimentalBCVApi
 public class KlibDump {
     internal var merger: KlibAbiDumpMerger = KlibAbiDumpMerger()
 
     /**
-     * Set of targets for which this dump contains declarations.
+     * Set of all targets for which this dump contains declarations.
+     *
+     * @sample samples.KlibDumpSamples.extractTargets
      */
     public val targets: Set<KlibTarget>
         get() = merger.targets
 
 
     /**
-     * Load a text dump for a target and merge it into this dump.
+     * Loads a textual KLib dump and merges it into this dump.
      *
-     * @throws IllegalArgumentException if the dump already has that target.
-     * @throws FileNotFoundException if [dump] does not exist.
+     * If a dump contains only a single target, it's possible to specify a custom configurable target name.
+     * Please refer to [KlibTarget.configurableName] for more details on the meaning of that name.
+     *
+     * By default, [configurableTargetName] is null and information about a target will be taken directly from
+     * the loaded dump.
+     *
+     * It's an error to specify non-null [configurableTargetName] for a dump containing multiple targets.
+     * It's also an error to merge dumps having some targets in common.
+     *
+     * @throws IllegalArgumentException if this dump and [dumpFile] shares same targets.
+     * @throws IllegalArgumentException if [dumpFile] contains multiple targets
+     * and [configurableTargetName] is not null.
+     * @throws FileNotFoundException if [dumpFile] does not exist.
+     *
+     * @sample samples.KlibDumpSamples.mergeDumps
      */
-    public fun merge(from: File, withTargetName: String? = null) {
-        if (!from.exists()) throw FileNotFoundException(from.absolutePath)
-        merger.merge(from, withTargetName)
+    public fun merge(dumpFile: File, configurableTargetName: String? = null) {
+        if (!dumpFile.exists()) throw FileNotFoundException(dumpFile.absolutePath)
+        merger.merge(dumpFile, configurableTargetName)
     }
 
     /**
-     * Merge [other] dump into this one.
+     * Merges [other] dump with this one.
+     *
+     * It's also an error to merge dumps having some targets in common.
+     *
+     * The operation does not modify [other].
+     *
+     * @throws IllegalArgumentException if this dump and [other] shares same targets.
+     *
+     * @sample samples.KlibDumpSamples.mergeDumpObjects
      */
     public fun merge(other: KlibDump) {
         val intersection = targets.intersect(other.targets)
@@ -44,7 +70,11 @@ public class KlibDump {
     }
 
     /**
-     * Remove all declarations that do not belong to the specified targets.
+     * Remove all declarations that do not belong to specified targets and remove these targets from the dump.
+     *
+     * All targets not stored within a dump will be ignored.
+     *
+     * @sample samples.KlibDumpSamples.extractTargets
      */
     public fun retain(targets: Iterable<KlibTarget>) {
         val toRemove = merger.targets.subtract(targets.toSet())
@@ -52,7 +82,11 @@ public class KlibDump {
     }
 
     /**
-     * Remove all declarations that do belong to the specified target.
+     * Remove all declarations that do belong to specified targets and remove these targets from the dump.
+     *
+     * All targets not stored within a dump will be ignored.
+     *
+     * @sample samples.KlibDumpSamples.mergeDumpObjects
      */
     public fun remove(targets: Iterable<KlibTarget>) {
         targets.forEach {
@@ -60,20 +94,61 @@ public class KlibDump {
         }
     }
 
+    /**
+     * Creates a copy of this dump.
+     */
     public fun copy(): KlibDump = KlibDump().also { it.merge(this) }
 
     /**
-     * Convert the dump back into a textual form.
+     * Serializes the dump and writes it to [to].
+     *
+     * @sample samples.KlibDumpSamples.mergeDumps
      */
     public fun saveTo(to: Appendable) {
         merger.dump(to)
     }
 
     public companion object {
+        /**
+         * Loads a dump from a textual form.
+         *
+         * If a dump contains only a single target, it's possible to specify a custom configurable target name.
+         * Please refer to [KlibTarget.configurableName] for more details on the meaning of that name.
+         *
+         * By default, [configurableTargetName] is null and information about a target will be taken directly from
+         * the loaded dump.
+         *
+         * It's an error to specify non-null [configurableTargetName] for a dump containing multiple targets.
+         *
+         * @throws IllegalArgumentException if [dumpFile] contains multiple targets
+         * and [configurableTargetName] is not null.
+         * @throws FileNotFoundException if [dumpFile] does not exist.
+         *
+         * @sample samples.KlibDumpSamples.mergeDumpObjects
+         */
         public fun from(dumpFile: File, configurableTargetName: String? = null): KlibDump {
             check(dumpFile.exists()) { "File does not exist: ${dumpFile.absolutePath}" }
             return KlibDump().apply { merge(dumpFile, configurableTargetName) }
         }
+
+        /**
+         * Dumps a public ABI of a klib represented by [klibFile] using [filters]
+         * and returns a [KlibDump] representing it.
+         *
+         * To control which declarations are dumped, [filters] could be used. By default, no filter will be applied.
+         *
+         * If a klib contains only a single target, it's possible to specify a custom configurable target name.
+         * Please refer to [KlibTarget.configurableName] for more details on the meaning of that name.
+         *
+         * By default, [configurableTargetName] is null and information about a target will be taken directly from
+         * the klib.
+         *
+         * It's an error to specify non-null [configurableTargetName] for a klib containing multiple targets.
+         *
+         * @throws IllegalArgumentException if [klibFile] contains multiple targets
+         * and [configurableTargetName] is not null.
+         * @throws FileNotFoundException if [klibFile] does not exist.
+         */
         public fun fromKlib(
             klibFile: File,
             configurableTargetName: String? = null,
@@ -90,8 +165,17 @@ public class KlibDump {
 }
 
 /**
- * Infer a possible public ABI for [target] using old merged dump [oldDump]
- * and set of [generatedDumps] generated for other targets.
+ * Infer a possible public ABI for [unsupportedTarget] as an ABI common across all [supportedTargetDumps].
+ * If there's an [oldMergedDump] consisting of declarations of multiple targets, including [unsupportedTarget],
+ * a portion of that dump specific to the [unsupportedTarget] will be extracted and merged to the common ABI
+ * build from [supportedTargetDumps].
+ *
+ * Returned dump contains only declarations for [unsupportedTarget].
+ *
+ * The function aimed to facilitate ABI dumps generation for targets that are not supported by a host compiler.
+ * In practice, it means generating dumps for Apple targets on non-Apple hosts.
+ *
+ * @sample samples.KlibDumpSamples.inferDump
  */
 @ExperimentalBCVApi
 public fun inferAbi(
@@ -117,6 +201,25 @@ public fun inferAbi(
     return commonDump
 }
 
+/**
+ * Dumps a public ABI of a klib represented by [klibFile] using [filters] and merges it into this dump.
+ *
+ * To control which declarations are dumped, [filters] could be used. By default, no filter will be applied.
+ *
+ * If a klib contains only a single target, it's possible to specify a custom configurable target name.
+ * Please refer to [KlibTarget.configurableName] for more details on the meaning of that name.
+ *
+ * By default, [configurableTargetName] is null and information about a target will be taken directly from
+ * the klib.
+ *
+ * It's an error to specify non-null [configurableTargetName] for a klib containing multiple targets.
+ * It's also an error to merge dumps having some targets in common.
+ *
+ * @throws IllegalArgumentException if this dump and [klibFile] shares same targets.
+ * @throws IllegalArgumentException if [klibFile] contains multiple targets
+ * and [configurableTargetName] is not null.
+ * @throws FileNotFoundException if [klibFile] does not exist.
+ */
 @ExperimentalBCVApi
 public fun KlibDump.mergeKlib(
     klibFile: File, configurableTargetName: String? = null,
